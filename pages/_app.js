bf3930b048dfb1f5c465711a7bed1b04d3421644
ronlobo <ronlobo.developer@gmail.com>
2021-07-15 09:38:15 +0530

    First commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'tailwindcss/tailwind.css';
+import { ProvideAuth } from '../utils/auth/useAuth';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ProvideAuth>
+      <Component {...pageProps} />
+    </ProvideAuth>
+  );
 }
 
-export default MyApp
+export default MyApp;
